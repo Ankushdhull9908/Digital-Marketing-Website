3a2a7f2db6f8264b2ae921f92e35ae2b91855240
Ankushdhull9908 <123265836+Ankushdhull9908@users.noreply.github.com>
2026-04-20 10:56:02 +0530

    Update Navbar.jsx

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/Context";
-// Icons for a professional touch
+// Icons for a professional touch.
 import { LayoutDashboard, LogOut, User, ChevronDown } from "lucide-react";
 
 const Navbar = () => {
