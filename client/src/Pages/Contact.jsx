6b68e3266aacbc5098105f194d3c5fe55e358c11
Ankushdhull9908 <123265836+Ankushdhull9908@users.noreply.github.com>
2026-05-14 20:31:26 +0530

    Update Contact.jsx

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -18,7 +18,6 @@ const Contact= () => {
   var API = "https://digital-marketing-temp.onrender.com/api/contact"
   ? "https://digital-marketing-temp.onrender.com/api/contact"
   : "http://localhost:5000/api/contact";
-
   const fadeInUp = {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6 } }
