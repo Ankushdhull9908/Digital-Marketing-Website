f762dc2e95cfb900baec8663b7a2043d851bbe59
Ankushdhull9908 <123265836+Ankushdhull9908@users.noreply.github.com>
2026-05-14 20:30:10 +0530

    Update AuthPage.jsx

--- client/src/Pages/AuthPage.jsx
+++ client/src/Pages/AuthPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/Context";
-
 function AuthPage() {
   const { login } = useAuth();
   const [isLogin, setIsLogin] = useState(true);
