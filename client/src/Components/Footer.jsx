faee9f029940622246e5318cecb436abbd39b933
Ankushdhull9908 <123265836+Ankushdhull9908@users.noreply.github.com>
2026-05-25 10:27:13 +0530

    Update Footer.jsx

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { 
   FaFacebookF,  
-  FaTwitter, 
+  FaTwitter,  
   FaLinkedinIn, 
   FaInstagram, 
   FaGithub,
