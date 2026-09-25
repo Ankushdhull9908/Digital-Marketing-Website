309b60fde6e1c9464254100e26b024bd39aab187
Ankushdhull9908 <ankushdhull9908@gmail.com>
2026-09-25 21:14:09 +0530

    renamed carrer file

--- server/server.js
+++ server/server.js
@@ -15,11 +15,13 @@ import uploadRoutes      from "./routes/upload.js";
 import resumeRoutes      from "./routes/resume.js"; // ← NEW
 import blogRoutes from "./routes/blogs.js";
 import industriesRoutes from './routes/industriesRoutes.js'
-import careerRoutes from "./routes/career.js";
+import careerRoutes from "./routes/careerPage.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
